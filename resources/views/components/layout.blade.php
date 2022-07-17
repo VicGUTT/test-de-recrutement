fc2f98f054df8f0233b96c98f77d1523201b9f65
Vic GUTT <guttvictor@yahoo.fr>
2022-07-17 09:09:46 +0200

    feat: front setup

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{  config('app.name') }}</title>
+
+    <link rel="stylesheet" href="{{ mix('dist/app.css') }}">
+    <script src="{{ mix('dist/app.js') }}" defer></script>
+</head>
+<body>
+    {{ $slot }}
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quis quae deleniti magnam voluptatum error et, modi tempora sed ducimus saepe. Quis delectus facilis vel saepe perferendis nihil praesentium aspernatur?
+    </p>
+</x-layout>
