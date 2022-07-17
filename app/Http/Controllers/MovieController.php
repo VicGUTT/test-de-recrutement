d4755f0b4a99ead36b6c17b16dc68cf0f297b749
Vic GUTT <guttvictor@yahoo.fr>
2022-07-17 11:17:16 +0200

    feat: ui update

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,13 +7,22 @@ namespace App\Http\Controllers;
 use App\Models\Movie;
 use Illuminate\View\View;
 use App\Http\Requests\MovieIndexRequest;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class MovieController
 {
     public function __invoke(MovieIndexRequest $request): View
     {
+        $term = $request->term();
+
         return view('pages.movies.index', [
-            'movies' => Movie::searchWithTerm($request->term() ?: '')->paginate(),
+            'movies' => Movie::query()->when(
+                $term,
+                fn (Builder $query): LengthAwarePaginator => $query->searchWithTerm($term)->paginate(),
+                fn (Builder $query): LengthAwarePaginator => $query->paginate(),
+            ),
+            'term' => $term,
         ]);
     }
 }
--- resources/views/pages/movies/index.blade.php
+++ resources/views/pages/movies/index.blade.php
@@ -1,46 +1,82 @@
 <x-layout>
-    <main style="max-width: 800px; margin-inline: auto; padding-block: 4rem; display: flex; flex-direction: column; gap: 1rem;">
-        <form>
-            <label for="search">Search</label>
-            <input id="search" type="text" name="q" value="{{ old('q') }}" placeholder="Taper le nom ou le genre d'un film..." autofocus>
+    <header class="container search-form-wrapper">
+        <form class="search-form">
+            <input
+                id="search"
+                class="search-field"
+                type="text"
+                name="q"
+                placeholder="Taper le nom ou le genre d'un film..."
+                value="{{ old('q', $term) }}"
+                autofocus
+            >
+            <label class="sr-only" for="search">Rechercher un film par son nom ou son genre</label>
 
-            @error('q')
-                <small aria-live="polite">{{ $message }}</small>
-            @enderror
+            <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
         </form>
 
-        <hr>
+        @error('q')
+            <small class="search-error" aria-live="polite">{{ $message }}</small>
+        @enderror
+    </header>
 
-        <section>
-            <ul>
+    <main class="container">
+        @if ($movies && $movies->isNotEmpty())
+            <section class="card-list">
                 @foreach ($movies as $movie)
-                    <li>
-                        <button>
-                            <img src="{{ $movie->image }}" width="100" height="100" alt="" aria-hidden="true">
-                            
-                            <strong>{{ $movie->name }}</strong>
-                            <small>{{ $movie->genre }}</small>
-                        </button>
-                    </li>
-                @endforeach
-            </ul>
-        </section>
+                    <article class="card movie-card">
+                        <img
+                            class="movie-card-poster"
+                            src="{{ $movie->image }}"
+                            alt="Image de couverture du film : {{ $movie->name }}"
+                            width="180"
+                            height="240"
+                        >
 
-        <section>
-            {{ $movies->withQueryString()->links() }}
-        </section>
+                        <div class="movie-card-content">
+                            <strong class="movie-card-name truncate">{{ $movie->name }}</strong>
+                            <small class="movie-card-genre">{{ $movie->genre }}</small>
+                        </div>
+                        
+                        <button class="movie-card-trigger">
+                            <span class="sr-only">Plus de détail sur {{ $movie->name }}</span>
+                        </button>
+                    </article>
+                @endforeach
+            </section>
+
+            <div class="movie-pagination-wrapper">
+                {{ $movies->withQueryString()->links() }}
+            </div>
+        @else
+            <section>
+                <p>
+                    Il semblerait qu'aucun film n'ait "<strong>{{ $term }}</strong>" comme nom et/ou genre.
+                </p>
+            </section>
+        @endif
     </main>
 
     <dialog></dialog>
 
+    <div class="decoration-background" aria-hidden="true">
+        <div class="container">
+            <div style="background: radial-gradient(105.68% 45.69% at 92.95% 50%, rgba(105, 244, 253, 0.5) 0%, rgba(160, 255, 244, 0.094) 53.91%, rgba(254, 216, 255, 0) 100%), radial-gradient(103.18% 103.18% at 90.11% 102.39%, rgb(201, 255, 242) 0%, rgba(230, 255, 250, 0) 100%), radial-gradient(90.45% 90.45% at 87.84% 9.55%, rgb(255, 210, 245) 0%, rgba(254, 219, 246, 0) 100%), linear-gradient(135.66deg, rgba(203, 185, 255, 0.8) 14.89%, rgba(216, 202, 254, 0) 74.33%); background-blend-mode: normal, normal, normal, normal, normal, normal; filter: blur(200px); border-radius: 15rem;"></div>
+        </div>
+    </div>
+
     <script>
-        document.querySelectorAll('li > button').forEach(btn => {
+        document.querySelectorAll('.movie-card > button').forEach(btn => {
             btn.addEventListener('click', () => {
-                const target = btn.cloneNode(true);
+                const parent = btn.parentElement.cloneNode(true);
 
                 const dialog = document.querySelector('dialog');
 
-                dialog.innerHTML = btn.outerHTML;
+                parent.querySelector('button').remove();
+
+                dialog.innerHTML = parent.outerHTML;
 
                 dialog.showModal();
             });
